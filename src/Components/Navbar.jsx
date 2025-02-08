9c564259a169e038cb1521d2619504bdd462944d
Anjum Shaikh <119291512+AnjumShaikh1508@users.noreply.github.com>
2025-02-09 00:26:45 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router";
 import '../CSS/Navbar.css';
+import { useFirebase } from "../Context/Firebase";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const { searchQuery, setSearchQuery } = useFirebase();
 
   return (
     <nav className="navbar">
@@ -11,7 +13,7 @@ const Navbar = () => {
         <NavLink to="/" className="logo">ConnectHub</NavLink>
 
         <div className="search-bar">
-          <input type="text" placeholder="Search..." />
+          <input type="text" placeholder="Search..." value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)}/>
           <button>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
